600a09aa7bfce17580946a471afe5bc35001ab3e
TomerDun <tomer@dunsky.name>
2025-07-13 15:26:35 +0300

    Finished factorChain

--- Easy/factorChain.js
+++ Easy/factorChain.js
@@ -18,8 +18,11 @@ factorChain([2, 4, 6, 7, 12]) ➞ false
 factorChain([10, 1]) ➞ false
 */
 
-function factorChain( /*args*/ ) {
-  //your code
+function factorChain( arr ) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] % arr[i-1] !== 0) return false;
+  }
+  return true;
 }
 
 exports.solution = factorChain;
